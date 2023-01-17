b66f692f292912346e36b029089be5d3af4274ff
Dott <chenyongarch@gmail.com>
2023-01-17 12:52:18 +0800

    fix: button animation and image routing

--- components/NavigationBar/components/NavigationButton.tsx
+++ components/NavigationBar/components/NavigationButton.tsx
@@ -21,9 +21,9 @@ const NavigationButton: React.FC<NavigationButtonProps> = ({ className, children
         <Tooltip content={`${name}·${shortcut}`}>
             <button className={twMerge(`flex h-10 w-10 rounded-2xl items-center justify-center bg-[var(--bg-sub)]\
             border-[1.5px] ${active ? "border-[var(--bg-border-strong)]" : "border-transparent"}\
-            hover:${active ? "bg-[var(--bg-shade)]" : "bg-[var(--bg-shade)]"}\
+            hover:bg-[var(--bg-shade)]\
             focus:border-[var(--bg-border-strong)] focus:outline-none
-            active:scale-90 transition duration-150 ease-out`, className)}
+            active:scale-90 active:translate-y-1 transition duration-300 ease-out`, className)}
             onClick={handleClick}>
                 <div className={`h-6 w-6 ${active ? "text-[var(--label-title)]" : "text-[var(--label-muted)]"}`}>{children}</div>
             </button>
