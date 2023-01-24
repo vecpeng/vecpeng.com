23f76d25d2194a5fec4b30673cc1d259ba1d4074
Dott <chenyongarch@gmail.com>
2023-01-24 12:38:02 +0800

    fix: bottom header height

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <>
       <div className="fixed top-0 w-full h-16 bg-[var(--bg-base)] gradient-mask-top-bottom z-20"></div>
-      <div className="fixed bottom-0 w-full h-[88px] bg-[var(--bg-base)] backdrop-blur-xl gradient-mask-bottom-top z-20"></div>
+      <div className="fixed bottom-0 w-full h-16 bg-[var(--bg-base)] backdrop-blur-xl gradient-mask-bottom-top z-20"></div>
     </>
   );
 };
