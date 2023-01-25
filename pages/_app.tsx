baa9889eb4942e839a637e1d77fc9a003da178f8
Dott <chenyongarch@gmail.com>
2023-01-25 16:35:35 +0800

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import * as ToastUI from "@radix-ui/react-toast";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -16,7 +17,10 @@ const App = ({ Component, pageProps }: AppProps) => {
       <TooltipUI.Provider delayDuration={800} skipDelayDuration={300}>
         <ToastUI.Provider duration={3000}>
           <Layout>
-            <Component {...pageProps} />
+            <>
+              <Component {...pageProps} />
+              <Analytics />
+            </>
           </Layout>
         </ToastUI.Provider>
       </TooltipUI.Provider>
